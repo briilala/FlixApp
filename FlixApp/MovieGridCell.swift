108a039dc18a1df3255a7468abbc5876a678d266
briilala <brisan.brown@gmail.com>
2020-09-16 23:03:16 -0400

    FlixApp part 2

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Brisan Brown on 9/16/20.
+//  Copyright © 2020 Brisan Brown. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
